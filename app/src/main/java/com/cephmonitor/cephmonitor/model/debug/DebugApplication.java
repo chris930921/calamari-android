0234acfde3f597ffef96471a2c4560997673a23c
ChrisKe <chris930921@gmail.com>
2015-06-14 23:38:40 +0800

    內部版本 0.7.0 新增除錯功能
    (1)增加DebugApplication類別，並再AndroidManifest中指定使用。
    (2)可自動上傳會造成崩潰的訊息到Google Doc上，會顯示時間、裝置、例外訊息。
    (3)會再額外儲存裝置中(SDcar)，儲存崩潰訊息，每一次崩潰會儲存成一檔案，放在SD卡下的Package名稱的資料夾內。

---
+++ app/src/main/java/com/cephmonitor/cephmonitor/model/debug/DebugApplication.java
@@ -0,0 +1,102 @@
+package com.cephmonitor.cephmonitor.model.debug;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.Environment;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.cephmonitor.cephmonitor.BuildConfig;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by User on 6/14/2015.
+ */
+public class DebugApplication extends Application {
+    protected Thread.UncaughtExceptionHandler customUncaughtExceptionHandler;
+    protected Thread.UncaughtExceptionHandler defaultUncaughtExceptionHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+        customUncaughtExceptionHandler = new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                writeToSdcardFile(e);
+                postToGoogleDoc(e);
+                defaultUncaughtExceptionHandler.uncaughtException(t, e);
+            }
+        };
+        Thread.setDefaultUncaughtExceptionHandler(customUncaughtExceptionHandler);
+    }
+
+    private void writeToSdcardFile(Throwable e) {
+        try {
+
+            File sdCardDir = new File(Environment.getExternalStorageDirectory() + "/" + getApplicationContext().getPackageName());
+            sdCardDir.mkdirs();
+
+            Calendar time = Calendar.getInstance();
+            File saveFile = new File(sdCardDir, "error_output_" + time.getTimeInMillis() + ".txt");
+            PrintWriter pw = new PrintWriter(saveFile);
+            pw.write(time.getTime().toString() + "\n");
+            pw.write(Build.BRAND + "\n" + Build.MODEL + "\n");
+            pw.write("==================================================================\n");
+            e.printStackTrace(pw);
+            pw.flush();
+            pw.close();
+        } catch (FileNotFoundException e1) {
+        }
+    }
+
+    private void postToGoogleDoc(Throwable e) {
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter psw = new PrintWriter(sw);
+            e.printStackTrace(psw);
+            String stackTrace = sw.toString();
+            GoogleDocPostRequest request = new GoogleDocPostRequest(
+                    stackTrace,
+                    Build.BRAND + "\n" + Build.MODEL + "\n",
+                    BuildConfig.IS_LOCALHOST + "",
+                    null, null
+            );
+            RequestQueue queue = Volley.newRequestQueue(DebugApplication.this);
+            queue.add(request);
+        } catch (Exception e1) {
+        }
+    }
+
+    class GoogleDocPostRequest extends StringRequest {
+        private String device;
+        private String errorMessage;
+        private String isLocalhost;
+
+        public GoogleDocPostRequest(String errorMessage, String device, String isLocalhost, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+            super(Method.POST, "https://docs.google.com/forms/d/1AxsF91Ext_fEBa9YPC3fia_ucotIeioZ63lyCtVw3wY/formResponse", listener, errorListener);
+            this.errorMessage = errorMessage;
+            this.device = device;
+            this.isLocalhost = isLocalhost;
+        }
+
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            HashMap<String, String> params = new HashMap<>();
+            params.put("entry.1347410436", device);
+            params.put("entry.1938478189", errorMessage);
+            params.put("entry.503708045", isLocalhost);
+            return params;
+        }
+    }
+}
